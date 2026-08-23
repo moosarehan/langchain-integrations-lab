4f25cb8c3ffd2ea213920d00ba856420373ed0a9
moosarehan <moosarehan4@gmail.com>
2026-08-23 19:31:37 +0500

    feat(structure-output): add string output parser demo

---
+++ langchain-structure-output/str_outputparser.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    temperature=0.7,
+)
+
+# Wrap with ChatHuggingFace
+model = ChatHuggingFace(llm=llm)
+
+template1=PromptTemplate(
+    template='write detailed report on {topic}',
+    input_variables=['topic']
+
+)
+
+
+template2=PromptTemplate(
+    template='write a short few lines summary on {text}',
+    input_variables=['text']
+
+)
+parser=StrOutputParser()
+chain=template1 | model | parser | template2 | model | parser
+
+response=chain.invoke({'topic':'blackhole'})
+print(response)
